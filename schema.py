4dea2b8f1a0ca7c06e7c5fa0d3d7a93f622da045
Eve-code93 <evelynejose1993@gmail.com>
2025-08-21 15:48:30 +0300

    fix files

--- schema.py
+++ schema.py
@@ -1,8 +1,1 @@
-import graphene
-
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
-
-
-schema = graphene.Schema(query=Query)
+from graphql_crm.schema import schema
